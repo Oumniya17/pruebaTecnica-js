4b819cc334a1b8e460b898501ea1ee30cbe8cc26
Oumniya17 <oumniyachahidi@gmail.com>
2026-01-28 03:01:27 +0100

    JS exercises finished

---
+++ index.js
@@ -0,0 +1,168 @@
+const people = [{ "id": "59308", "employee_name": "VojtaVyhnalAG", "employee_salary": "50", "employee_age": "25", "profile_image": "" }, { "id": "59310", "employee_name": "VojtaVyhnal2", "employee_salary": "50", "employee_age": "25", "profile_image": "" }, { "id": "59311", "employee_name": "David", "employee_salary": "123", "employee_age": "23", "profile_image": "" }, { "id": "59313", "employee_name": "PPstit22", "employee_salary": "12322", "employee_age": "22", "profile_image": "" }, { "id": "59315", "employee_name": "sudhakark", "employee_salary": "123", "employee_age": "23", "profile_image": "" }, { "id": "59318", "employee_name": "test1ing02", "employee_salary": "78", "employee_age": "45", "profile_image": "" }, { "id": "59322", "employee_name": "test1ing023", "employee_salary": "78", "employee_age": "45", "profile_image": "" }, { "id": "59323", "employee_name": "test1ing0234", "employee_salary": "78", "employee_age": "45", "profile_image": "" }, { "id": "59326", "employee_name": "yana11", "employee_salary": "1000", "employee_age": "22", "profile_image": "" }, { "id": "59329", "employee_name": "Admin", "employee_salary": "20000", "employee_age": "30", "profile_image": "" }, { "id": "59337", "employee_name": "Gizi", "employee_salary": "144000", "employee_age": "30", "profile_image": "" }, { "id": "59342", "employee_name": "Rojan", "employee_salary": "20000", "employee_age": "25", "profile_image": "" }, { "id": "59345", "employee_name": "Sanam", "employee_salary": "12345", "employee_age": "25", "profile_image": "" }, { "id": "59350", "employee_name": "murali1", "employee_salary": "123", "employee_age": "23", "profile_image": "" }, { "id": "59358", "employee_name": "Jakab", "employee_salary": "140550", "employee_age": "39", "profile_image": "" }, { "id": "59362", "employee_name": "yourValue 6", "employee_salary": "0", "employee_age": "23", "profile_image": "" }, { "id": "59363", "employee_name": "WRTEST63", "employee_salary": "431", "employee_age": "23", "profile_image": "" }, { "id": "59364", "employee_name": "WRTEST64", "employee_salary": "431", "employee_age": "23", "profile_image": "" }, { "id": "59365", "employee_name": "1557929525873", "employee_salary": "123", "employee_age": "23", "profile_image": "" }, { "id": "59370", "employee_name": "Chcek", "employee_salary": "123", "employee_age": "123", "profile_image": "" }, { "id": "59374", "employee_name": "1557930156346", "employee_salary": "123", "employee_age": "23", "profile_image": "" }, { "id": "59375", "employee_name": "testing", "employee_salary": "1235", "employee_age": "23", "profile_image": "" }, { "id": "59380", "employee_name": "", "employee_salary": "0", "employee_age": "0", "profile_image": "" }, { "id": "59381", "employee_name": "Dibash", "employee_salary": "40000", "employee_age": "24", "profile_image": "" }, { "id": "59385", "employee_name": "name", "employee_salary": "111111", "employee_age": "25", "profile_image": "" }, { "id": "59387", "employee_name": "Rama4", "employee_salary": "123", "employee_age": "23", "profile_image": "" }, { "id": "59388", "employee_name": "Jol\u00e1n", "employee_salary": "144006", "employee_age": "20", "profile_image": "" }, { "id": "59389", "employee_name": "Jolan", "employee_salary": "144006", "employee_age": "20", "profile_image": "" }, { "id": "59391", "employee_name": "Samyamasdsad", "employee_salary": "150222", "employee_age": "22", "profile_image": "" }, { "id": "59393", "employee_name": "1557931642839", "employee_salary": "123", "employee_age": "23", "profile_image": "" }, { "id": "59394", "employee_name": "s", "employee_salary": "15000", "employee_age": "20", "profile_image": "" }, { "id": "59395", "employee_name": "Karim", "employee_salary": "33446", "employee_age": "28", "profile_image": "" }, { "id": "59399", "employee_name": "Tom", "employee_salary": "0", "employee_age": "21", "profile_image": "" }, { "id": "59401", "employee_name": "Susan", "employee_salary": "1000", "employee_age": "12", "profile_image": "" }, { "id": "59402", "employee_name": "Bhuwan", "employee_salary": "75000", "employee_age": "22", "profile_image": "" }, { "id": "59403", "employee_name": "test", "employee_salary": "123", "employee_age": "23", "profile_image": "" }, { "id": "59404", "employee_name": "Harpreet", "employee_salary": "123", "employee_age": "24", "profile_image": "" }, { "id": "59406", "employee_name": "Geza397871757", "employee_salary": "100990", "employee_age": "35", "profile_image": "" }, { "id": "59407", "employee_name": "Bhuwan Bhatt", "employee_salary": "230000", "employee_age": "24", "profile_image": "" }, { "id": "59411", "employee_name": "Joska7385", "employee_salary": "100000", "employee_age": "22", "profile_image": "" }, { "id": "59413", "employee_name": "tester", "employee_salary": "123", "employee_age": "23", "profile_image": "" }, { "id": "59419", "employee_name": "SomeName", "employee_salary": "99999", "employee_age": "16", "profile_image": "" }, { "id": "59423", "employee_name": "new", "employee_salary": "2000", "employee_age": "25", "profile_image": "" }, { "id": "59426", "employee_name": "Sagun Shrestha", "employee_salary": "51232", "employee_age": "20", "profile_image": "" }, { "id": "59427", "employee_name": "SampleAPItest", "employee_salary": "12345", "employee_age": "30", "profile_image": "" }, { "id": "59432", "employee_name": "Trang", "employee_salary": "123", "employee_age": "23", "profile_image": "" }, { "id": "59441", "employee_name": "employee", "employee_salary": "25464", "employee_age": "32", "profile_image": "" }, { "id": "59442", "employee_name": "1557934135204", "employee_salary": "123", "employee_age": "23", "profile_image": "" }, { "id": "59446", "employee_name": "over9000", "employee_salary": "99000", "employee_age": "20", "profile_image": "" }, { "id": "59451", "employee_name": "JackJack", "employee_salary": "25000", "employee_age": "25", "profile_image": "" }, { "id": "59456", "employee_name": "testapa", "employee_salary": "123", "employee_age": "23", "profile_image": "" }, { "id": "59459", "employee_name": "jknadsakjsdjknas", "employee_salary": "123", "employee_age": "23", "profile_image": "" }, { "id": "59482", "employee_name": "Manish Mhr", "employee_salary": "4400", "employee_age": "34", "profile_image": "" }, { "id": "59490", "employee_name": "milin", "employee_salary": "234", "employee_age": "24", "profile_image": "" }, { "id": "59492", "employee_name": "dhyana", "employee_salary": "25", "employee_age": "24", "profile_image": "" }, { "id": "59515", "employee_name": "Jimmy", "employee_salary": "100000", "employee_age": "26", "profile_image": "" }, { "id": "59516", "employee_name": "Alisha", "employee_salary": "30000", "employee_age": "20", "profile_image": "" }, { "id": "59519", "employee_name": "Application ", "employee_salary": "222", "employee_age": "22", "profile_image": "" }, { "id": "59520", "employee_name": "1557936662915", "employee_salary": "123", "employee_age": "23", "profile_image": "" }, { "id": "59521", "employee_name": "Will chams", "employee_salary": "70000", "employee_age": "34", "profile_image": "" }, { "id": "59522", "employee_name": "1557936787344", "employee_salary": "123", "employee_age": "23", "profile_image": "" }, { "id": "59524", "employee_name": "abc", "employee_salary": "25", "employee_age": "24", "profile_image": "" }, { "id": "59526", "employee_name": "ally", "employee_salary": "1500", "employee_age": "25", "profile_image": "" }]
+
+/* =========================================================== */
+/*     Complete each function following the instructions       */
+/*            Use ES6 / ES7 standards if possible              */
+/* =========================================================== */
+
+/* === return how many employees have salary above 100K - 1 point === */
+
+const rich = (people) =>
+  people.filter(p => Number(p.employee_salary) > 100000).length;
+
+
+
+// rich(people)
+
+/* === add gender property to each employee, use female/male value randomly and return people - 1 point === */
+
+const addGender = (people) =>
+  people.map(p => ({
+    ...p,
+    gender: Math.random() < 0.5 ? "male" : "female",
+  }));
+
+
+//addGender(people)
+
+/* === return the average salary in employees array, two decimals, in US dollars - 1 point === */
+
+const averageSalary = (people) => {
+  const total = people.reduce(
+    (sum, p) => sum + Number(p.employee_salary),
+    0
+  );
+
+  return `$${(total / people.length).toFixed(2)}`;
+};
+
+
+/* === return if any employee has a salary of 0 - 1 point === */
+
+const isZeroSalary = (people) =>
+  people.some(p => Number(p.employee_salary) === 0);
+
+
+// isZeroSalary(people)
+
+/* === find all employees with salary 0, add them 'unpaid_leave' property setted to true - 1 point === */
+
+const unpaidLeave = (people) =>
+  people.map(p =>
+    Number(p.employee_salary) === 0
+      ? { ...p, unpaid_leave: true }
+      : p
+  );
+
+
+// unpaidLeave(people)
+
+/* === return true if all employees have the profile image field empty - 1 point === */
+
+const findProfilePic = (people) =>
+  people.every(p => p.profile_image === "");
+
+
+// findProfilePic(people)
+
+/* === Return people array with all names uppercased, without modifying the original array - 2 points === */
+
+const uppercasedEmployees = (people) =>
+  people.map(p => ({
+    ...p,
+    employee_name: p.employee_name.toUpperCase(),
+  }));
+
+
+// uppercasedEmployees(people)
+
+/* === return the most repeated letter in the string - 3 points === */
+
+const str = "qijsrtmkrjitkfptyjdebnoxjdqcketovtcmzdgg";
+
+const mostRepeatedLetter = (str) =>
+  Object.entries(
+    [...str].reduce((acc, char) => {
+      acc[char] = (acc[char] || 0) + 1;
+      return acc;
+    }, {})
+  ).sort((a, b) => b[1] - a[1])[0][0];
+
+
+//mostRepeatedLetter(str)
+
+/* === delete repeated elements and return an array. No loops allowed - 1 point === */
+
+const arr = [1, 2, 1, 3, 4, 5, 6, 65, 46, 46, 87, 65, 65, 122, 122, 122];
+
+const removeDups = (arr) => [...new Set(arr)];
+
+
+// removeDups(arr)
+
+/* === return the array with the names capitalized (ej. 'Wendell'), it's not allowed using loops (for, while, forEach, map, etc.) - 2 points === */
+
+const names = [
+  "wendell",
+  "asha",
+  "clare",
+  "rosalia",
+  "arnoldo",
+  "jeanice",
+  "lizzette",
+  "vernell",
+  "dwayne",
+  "greg",
+  "garnet",
+  "ariane",
+  "melinda",
+  "delena",
+  "sophia",
+  "gregoria",
+  "jinny",
+  "hyun",
+  "senaida",
+  "freida",
+];
+
+const capitalizeNames = (names) =>
+  names.reduce(
+    (acc, n) =>
+      acc.concat(n.charAt(0).toUpperCase() + n.slice(1).toLowerCase()),
+    []
+  );
+
+
+//capitalizeNames(names)
+
+/* === explain the undefined brand - 1 point === */
+
+const contextProblem = () => {
+  const car = {
+    brand: "Audi",
+    printBrand() {
+      return "The brand " + this.brand + " is from Germany";
+    },
+  };
+
+  console.log(car.printBrand());
+
+  // solution:
+  const myFunction = car.printBrand.bind(car);
+  console.log(myFunction());
+};
+
+
+// contextProblem()
+
+// Al separar la función del objeto, se pierde el "this", por eso brand es undefined
+
+// Testing all functions
+console.log(rich(people));
+console.log(averageSalary(people));
+console.log(isZeroSalary(people));
+console.log(findProfilePic(people));
+console.log(removeDups(arr));
+console.log(capitalizeNames(names));
+console.log(mostRepeatedLetter(str));
+console.log(contextProblem());
